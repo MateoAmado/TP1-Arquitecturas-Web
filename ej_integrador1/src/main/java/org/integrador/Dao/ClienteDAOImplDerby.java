fbce23f5bb1e4a04062836c9533ed9e320db27cb
Gonzalo <gonzalohellers@gmail.com>
2024-08-24 18:14:37 -0300

    Clientes Derby

--- ej_integrador1/src/main/java/org/integrador/Dao/ClienteDAOImplDerby.java
+++ ej_integrador1/src/main/java/org/integrador/Dao/ClienteDAOImplDerby.java
@@ -1,18 +1,39 @@
 package org.integrador.Dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.integrador.Modelo.Cliente;
+import org.integrador.Util.ConnectionFactory;
 
 public class ClienteDAOImplDerby implements ClienteDAO {
+    private static Connection connection= ConnectionFactory.instance().connect(ConnectionFactory.DERBY);
+
     @Override
     public void crear_tabla() {
-
+        try {
+            String table= "CREATE TABLE Cliente (idCliente int, nombre varchar(500), email varchar(150), PRIMARY KEY(idCliente))";
+            connection.prepareStatement(table).execute();
+            connection.commit();
+            ConnectionFactory.instance().disconnect();
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void insertar(Cliente cliente) {
-
+        String sql = " INSERT INTO cliente (idCliente, nombre, email) VALUES (?,?,?)";
+        try(PreparedStatement stmt = this.connection.prepareStatement(sql)){
+            stmt.setInt(1, cliente.getIdCliente();
+            stmt.setInt(2, cliente.getNombre();
+            stmt.setInt(3, cliente.getEmail();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
--- ej_integrador1/src/main/java/org/integrador/Dao/FacturaDAOImplMySql.java
+++ ej_integrador1/src/main/java/org/integrador/Dao/FacturaDAOImplMySql.java
@@ -1,19 +1,38 @@
 package org.integrador.Dao;
 
 import org.integrador.Modelo.Factura;
+import org.integrador.Util.ConnectionFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class FacturaDAOImplMySql implements FacturaDAO{
+    private static Connection connection= ConnectionFactory.instance().connect(ConnectionFactory.DERBY);
 
     @Override
-    public void crear_tabla() {
-
+    public  void crear_tabla(){
+        try {
+            String table= "CREATE TABLE Factura (idFactura INT, idCliente INT, PRIMARY KEY (idFactura), FOREIGN KEY(cliente_idCliente) REFERENCES cliente (idCliente))";
+            connection.prepareStatement(table).execute();
+            connection.commit();
+            ConnectionFactory.instance().disconnect();
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void insertar(Factura factura) {
-
+        String sql = "INSERT INTO factura_producto (idFactura, idCliente) VALUES (?,?)";
+        try(PreparedStatement stmt = this.connection.prepareStatement(sql)){
+            stmt.setInt(1, factura.getIdFactura());
+            stmt.setInt(2, factura.getIdCliente());
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
