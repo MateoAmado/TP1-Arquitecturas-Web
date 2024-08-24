2eda48aba152e67133f52248c588e3b5979ef69a
Gonzalo <gonzalohellers@gmail.com>
2024-08-24 16:42:11 -0300

    Primer main

--- ej_integrador1/src/main/java/org/integrador/Main/Main.java
+++ ej_integrador1/src/main/java/org/integrador/Main/Main.java
@@ -1,7 +1,13 @@
 package org.integrador.Main;
+import org.integrador.Dao.DAOFactory
+import org.integrador.Modelo.Cliente;
+import org.integrador.Util.ConnectionFactory;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DAOFactory dao_factory = DAOFactory.getInstance();
+        dao_factory.getClienteDAO(ConnectionFactory.DERBY).crear_tabla();
+        Cliente jugador = dao_factory.getClienteDAO(ConnectionFactory.DERBY).buscar_por_nombre("Nelson");
+        System.out.println(jugador.toString());
     }
 }
